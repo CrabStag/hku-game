0de50a2b75f1a24b68b2b038b4c990925993abe4
CrabStag <57531724+CrabStag@users.noreply.github.com>
2019-11-16 15:00:22 +0100

    toegevoegd hero smash en enemy

---
+++ Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int health;
+   
+
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+    	if(health <= 0){
+    		Destroy(this.gameObject);
+
+    	}
+        
+    } 
+    public void TakeDamage(int damage){
+    	health -= damage;
+    	Debug.Log ("damage TAKEN");
+
+    }
+}
---
+++ HeroSmash.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroSmash : MonoBehaviour
+{
+	private Rigidbody2D m_Rigidbody2D;
+    private bool attackIngedrukt = false;
+    private float timeBtwAttack = 0;
+    public float startTimeBtwAttack; 
+
+    public Transform attackPos;
+    //public Layermask whatIsEnemies;
+    public float attackRange;
+    public int damage; 
+    
+    void Update()
+    {
+        if(timeBtwAttack <=0) {
+        	//then you can attack
+        	if(Input.GetKey(KeyCode.X)) {
+ 				timeBtwAttack = startTimeBtwAttack;
+ 				// do attack here...
+ 				Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange);
+ 				for (int i = 0; i < enemiesToDamage.Length; i++) {
+ 					enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
+
+ 				}
+        	}
+
+        	
+
+        } else {
+        	timeBtwAttack -= Time.deltaTime;
+        }
+    }
+    void OnDrawGizmosSelected(){
+    	Gizmos.color = Color.red;
+    	Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+}
